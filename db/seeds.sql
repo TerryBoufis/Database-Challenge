6587437b2f7e44e0db21ffb97b5233117cf05cc5
Terry Boufis <terryboufis@gmail.com>
2022-11-21 15:52:52 -0600

    created database and tables. Started seed.sql formating

---
+++ db/seeds.sql
@@ -0,0 +1,19 @@
+INSERT INTO department (name)
+VALUES (""),
+       (""),
+       (""),
+       (""),
+       ("");
+
+INSERT INTO role (id, title, salary, department_id)
+VALUES (1, ""),
+       (2, ""),
+       (1, ""),
+       (3, ""),
+       (5, ""),
+       (1, ""),
+       (4, ""),
+       (5, "");
+
+INSERT INTO role (id, first_name, last_name, role_id, manager_id)
+VALUES (),
